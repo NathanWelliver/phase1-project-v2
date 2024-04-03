c57b1e5d35b62e649ac5b5cf37740341093ed83a
Nathan Welliver <nwelliver.564@gmail.com>
2024-04-02 20:21:38 -0400

    updated to show list of songs in rotation

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     /// / QUERY SELECTORS ////
-    
     // Music Player
     const musicPlayer = document.querySelector('.music__player');
     const playBtn =  document.querySelector('#play');
     const prevBtn = document.querySelector('#prev');
     const nextBtn = document.querySelector('#next');
     const audio = document.querySelector('#audio');
-    // Speed Adjustment
-    const speedIndicator = document.querySelector('.speed');
-    const speedNumber = document.querySelector('.speed p');
-    const speedOptions = [1.0, 1.5, 2.0, 0.75];
-    let speedIndex = 0;
-
-    // Progress Bar
-    const progressContainer = document.querySelector('.music__player--progress');
-    const progress =  document.querySelector('.progress');
 
     // Title and Image
     const audioTitle = document.querySelector('.music__title');
     const audioImage = document.querySelector('.music__img');
-
+    
     // Songs
     let songs;
     let songIndex = 0;
@@ -29,15 +19,30 @@ document.addEventListener('DOMContentLoaded', () => {
     const newSongContainer = document.querySelector('.music__form--container')
     const newSongBtn = document.querySelector('#new__music--button');
     let addSong = false;
-
+    // music list
+    const musicList = document.querySelector('#music_list')
     /// / FUNCTIONS ////
 
-    // update UI with current song
     function loadSong(song) {
         audioTitle.innerText = song.title;
         audio.src = `${song.audio}`;
         audioImage.style.backgroundImage = `url('${song.cover}')`;
     }
+
+    // display songs in a list
+    function loadSongList(song) {
+        const list = document.createElement('ul')
+        list.id = song.id
+        list.innerHTML = `
+            <h4>${song.title}</h4>
+            <audio src="${song.audio}"></audio>
+            <h5>${song.author}</h5>
+            <br>
+        `;
+        musicList.appendChild(list);
+        console.log()
+    }
+
     // check if song is playing
     function isAudioPlaying() {
         return musicPlayer.classList.contains('playing');
@@ -48,9 +53,6 @@ document.addEventListener('DOMContentLoaded', () => {
         musicPlayer.classList.add('playing');
         playBtn.querySelector('i').classList.remove('ph-play-circle');
         playBtn.querySelector('i').classList.add('ph-pause-circle');
-        //PLAYBACK RATE
-        audio.playbackRate = `${speedOptions[speedIndex]}`;
-        
         audio.play();
     }
 
@@ -73,7 +75,13 @@ document.addEventListener('DOMContentLoaded', () => {
             })
             .then((data) => {
                 songs = data;
-                loadSong(songs[songIndex]);
+
+                loadSong(songs[songIndex])
+
+                songs.forEach(song => {
+                    loadSongList(song)
+                });
+                
             })
             .catch((error) => {
                 console.error('There has been a problem with your fetch operation:', error);
@@ -88,7 +96,6 @@ document.addEventListener('DOMContentLoaded', () => {
             songIndex =  songs.length - 1;
         }
         loadSong(songs[songIndex]);
-        progress.style.width = '0%';
         isAudioPlaying() === true ? playAudio() : pauseAudio();
     }
 
@@ -99,33 +106,7 @@ document.addEventListener('DOMContentLoaded', () => {
             songIndex = 0;
         }
         loadSong(songs[songIndex]);
-        progress.style.width = '0%';
         isAudioPlaying() === true ? playAudio() : pauseAudio();
-    }
-
-    // update progress bar width
-    function updateProgressBar(e){
-        const {duration, currentTime} = e.srcElement;
-        const progressPercentage = (currentTime / duration) * 100;
-        progress.style.width = `${progressPercentage}%`
-    }
-
-    // move audio to where you click on playing audio track
-    function updateProgressBarPlayPosition(e){
-        const width = this.clientWidth;
-        const clickX = e.offsetX;
-        const { duration } = audio;
-        audio.currentTime = (clickX / width) * duration;
-    }
-
-    // update speed indicator
-    function updateSpeedIndicator(){
-        speedIndex += 1;
-        if(speedIndex > speedOptions.length - 1) {
-            speedIndex = 0;
-        } 
-        speedNumber.textContent = `${speedOptions[speedIndex]}x`
-        playAudio();
     }
 
     // Input form for new songs
@@ -137,13 +118,13 @@ document.addEventListener('DOMContentLoaded', () => {
         // Extracting data from the form
         const title = addMusicForm.querySelector('[name="new__music--title"]').value;
         const audioLink = addMusicForm.querySelector('[name="new__music--audio--link"]').value;
-        const coverLink = addMusicForm.querySelector('[name="new__music--img"]').value;
+        const author = addMusicForm.querySelector('[name="new__music--author"]').value;
 
         // Creating the song object
         const newSong = {
             title,
             audio: audioLink,
-            cover: coverLink
+            author: author,
         };
             
         fetch('http://localhost:3000/songs', {
@@ -161,21 +142,16 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     });
 
-    /// / EVENT LISTENERS ////
 
+    /// / EVENT LISTENERS ////
+    
     // Play, prev, next btns
     playBtn.addEventListener('click', () => {
         isAudioPlaying() ? pauseAudio() : playAudio();
     });
     prevBtn.addEventListener('click', prevSong);
     nextBtn.addEventListener('click', nextSong);
-    // update speed indicator
-    speedIndicator.addEventListener('click', updateSpeedIndicator);
-
-    // progress bar updates
-    audio.addEventListener('timeupdate', updateProgressBar);
-    progressContainer.addEventListener('click', updateProgressBarPlayPosition);
-
+    
     // Expand new song form button
     newSongBtn.addEventListener('click', () => {
         addSong = !addSong;
